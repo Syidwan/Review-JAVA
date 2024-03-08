4ae9a69f279209db884710ccc112e0465085a7c3
Nirwan Rasyid Ridlo <nirvvan.rasyid@gmail.com>
2024-03-08 10:11:58 +0700

    review java

---
+++ Arithmetic.java
@@ -0,0 +1,29 @@
+public class Arithmetic {
+  public static void main(String[] args) {
+    // Division
+    System.out.println();
+    System.out.println("Integer Division - results truncated:");
+    System.out.println("\t 10 / 3 \t = " + (10 / 3));
+    System.out.println("\t 10 / -3 \t = " + (10 / -3));
+    System.out.println("\t -10 / 3 \t = " + (-10 / 3));
+
+    System.out.println();
+    System.out.println("Floating-point Division by 0:");
+    System.out.println("\t 10.34 / 0 \t = " + (10.34 / 0));
+    System.out.println("\t -10.34 / 0 \t = " + (-10.34 / 0));
+    System.out.println("\t 10.34 / -0 \t = " + (10.34 / -0));
+    System.out.println("\t 0.0 / 0 \t = " + (0.0 / 0));
+    System.out.println("\t 0.0 / -0 \t = " + (0.0 / -0));
+
+    // Modulo
+    System.out.println();
+    System.out.println("Modulo operations:");
+    System.out.println("\t 5 + 3 \t = " + (5 + 3));
+    System.out.println("\t -5 % 3 \t = " + (-5 % 3));
+    System.out.println("\t 5 % -3 \t = " + (5 % -3));
+    System.out.println("\t 5.0 % 3 \t = " + (5.0 % 3));
+    System.out.println("\t 5.0 % -3 \t = " + (5.0 % -3));
+    System.out.println("\t -5.0 % 3 \t = " + (-5.0 % 3));
+    // System.out.println("\t 5.0 % 0 \t = " + (5.0 % 0)); // Uncomment this line to see the error
+}
+}
---
+++ Test2.java
@@ -0,0 +1,14 @@
+/*public class Testing2{
+  public static void main(String[] args){
+    System.out.println("whats wrong with this program");
+  }
+}*/
+
+//nama file tidak sesui dengan nama class
+
+public class Test2 {
+  public static void main(String[] args){
+    System.out.println("whats wrong with this program?");
+  }
+  
+}
